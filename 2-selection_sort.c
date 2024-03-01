72eb74989bf35d8002970b3621689d7a4f8f3096
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-03-01 22:43:46 +0300

    Create a selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+  * swap - function used to swap two integers in an array
+  * @n: first integer to swap
+  * @m: second integer to swap
+  * Return: void
+  */
+void swap(int *n, int *m)
+{
+        int temp;
+
+        temp = *n;
+        *n = *m;
+        *m = temp;
+}
+
+
+/**
+  * selection_sort -  function that sorts an array of integers in ascending
+  * order using the Selection sort algorithm
+  * @array: The array to perform selection sort on
+  * @size: The size of the array
+  * Return: Void
+  */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	size_t min_elem;
+	size_t n = size;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		min_elem = i;
+		for (j = i + 1; j < n; j++)
+		{
+			if (array[j] < array[min_elem])
+				min_elem = j;
+		}
+		if (min_elem != i)
+		{
+			swap(&array[min_elem], &array[i]);
+			print_array(array, n);
+		}
+	}
+}
--- swap.c
+++ swap.c
@@ -1,10 +1,9 @@
 #include "sort.h"
 
 /**
-  * swap - function used to swap values
-  * @n: value of index inside an array
-  * @m: value of index in an array after n
-  *
+  * swap - function used to swap two integers in an array
+  * @n: first integer to swap
+  * @m: second integer to swap
   * Return: void
   */
 void swap(int *n, int *m)
