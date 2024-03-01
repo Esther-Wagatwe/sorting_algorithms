e55423b1b1e4d52452d5f73d7157ed38e51fde17
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-03-01 17:28:26 +0300

    Correct bubbly

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,7 +12,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, temp;
 	bool swapped;
 
-	for(i = 0; i < size - 1; i++)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		swapped = false;
 		for (j = 0; j < size - i - 1; j++)
@@ -20,7 +23,7 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j +1];
+				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
 				swapped = true;
