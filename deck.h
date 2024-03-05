6b90139d40f860804cc5c5bc33037b4ff4a0298d
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-03-06 00:12:31 +0300

    Betty corrections

--- deck.h
+++ deck.h
@@ -4,12 +4,19 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+/**
+ * enum kind_e - Represents the four standard playing card suits.
+ * @SPADE:   Spade suit.
+ * @HEART:   Heart suit.
+ * @CLUB:    Club suit.
+ * @DIAMOND: Diamond suit.
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -21,8 +28,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -34,9 +41,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
